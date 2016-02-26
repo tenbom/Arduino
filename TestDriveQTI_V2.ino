b268a46f2ae6fd573d5ad1e4ce7857a8a2cf0966
Richard C <higgsbosonseeker@gmail.com>
2016-02-25 17:29:36 -0800

    Added files via upload

--- TestDriveQTI_V2.ino
+++ TestDriveQTI_V2.ino
@@ -1,15 +1,15 @@
 //from front of sensor
 //5v, ground, signal
 //2/24/2016 EDIT
-
+//Make sure the ground all goes to the same place
 //#include "DualVNH5019MotorShield.h"
 //DualVNH5019MotorShield md;
 //Pins for the sensors=======================================
-#define Pin2 20
-#define Pin3 22
-#define Pin4 24
-#define Pin5 26
-#define Pin6 28
+#define Pin2 22 //left side
+#define Pin3 24
+#define Pin4 26
+#define Pin5 28
+#define Pin6 30 // right side
 //===========================================================
 uint64_t currentTime = 0;
 uint8_t state = 0;
@@ -20,11 +20,13 @@ void setup() {
 
 void loop() {
   currentTime = millis();           // Initialize Timer
-  Serial.println(RCTime(Pin2));	    // Connect to pin 2, display results
+  //Serial.println(RCTime(Pin2));	    // Connect to pin 2, display results
   //Serial.println(RCTime(Pin3));   // Same for these
   //Serial.println(RCTime(Pin4));
   //Serial.println(RCTime(Pin5));
   //Serial.println(RCTime(Pin6));
+  sensorState();
+  //delay(50);
 }
 
 int RCTime(int sensorIn){         //
@@ -32,10 +34,10 @@ int RCTime(int sensorIn){         //
    digitalWrite(sensorIn, HIGH);  // Pin HIGH (discharge capacitor)
    pinMode(sensorIn, INPUT);      // Make pin INPUT
    digitalWrite(sensorIn, LOW);   // Turn off internal pullups
-   if(digitalRead(sensorIn)){     // Pin goes LOW which means its on the black line
+   if (!digitalRead(sensorIn)) {  // Pin goes HIGH which means its not on the black line
       return 1;
    }
-   if (!digitalRead(sensorIn)) {  // Pin goes HIGH which means its not on the black line
+   if(digitalRead(sensorIn)){     // Pin goes LOW which means its on the black line
       return 0;
    }
 }
@@ -47,7 +49,10 @@ int RCTime(int sensorIn){         //
   a for loop that would ruin the continuity of multi tasking for the robot. 
 */
 long sensorState() { 
+  //int temp = 0;
   state = ((RCTime(Pin6)*pow(2,4)) + (RCTime(Pin5)*pow(2,3)) + (RCTime(Pin4)*pow(2,2)) + (RCTime(Pin3)*pow(2,1)) + RCTime(Pin2));
+  Serial.println(state);
+  //temp = state;
   return state;
 }
 
